48a142f2e372ff249e3d3a23f0da6323ba482e15
Szorad <david.szorad@accenture.com>
2017-09-12 12:00:10 +0200

    Joined Report and Resume into Document Parent Class for better readability

--- Document.cs
+++ Document.cs
@@ -25,4 +25,34 @@ namespace FactoryMethodPattern
         // Factory Method
         public abstract void CreatePages();
     }
+
+    /// <summary>
+    /// A 'ConcreteCreator' class
+    /// </summary>
+    class Report : Document
+    {
+        // Factory Method implementation
+        public override void CreatePages()
+        {
+            Pages.Add(new IntroductionPage());
+            Pages.Add(new ResultsPage());
+            Pages.Add(new ConclusionPage());
+            Pages.Add(new SummaryPage());
+            Pages.Add(new BibliographyPage());
+        }
+    }
+
+    /// <summary>
+    /// A 'ConcreteCreator' class
+    /// </summary>
+    class Resume : Document
+    {
+        // Factory Method implementation
+        public override void CreatePages()
+        {
+            Pages.Add(new SkillsPage());
+            Pages.Add(new EducationPage());
+            Pages.Add(new ExperiencePage());
+        }
+    }
 }
